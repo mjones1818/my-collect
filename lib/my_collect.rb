d6dc0b27b46c546b5bd16c5df2e90f0d724f4259
mjones1818 <michael.jones0532@gmail.com>
2020-10-02 13:45:14 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+require 'pry'
 
+def my_collect (array)
+  i = 0
+  results = []
+  while i < array.length
+    results << yield(array[i])
+    i += 1
+  end
+  results
+end
 
+puts my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) {|name| name.split(" ").first}
